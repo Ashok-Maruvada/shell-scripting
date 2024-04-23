63ec52468574d953c37a5434f642151aa48d72aa
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-04-23 09:46:52 +0530

    using colors

---
+++ install-packages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+userid=$(id -u)
+timestamp=$(date +%f+%H+%M+%S)
+LOGFILE=$(echo $0 | cut -d "." -f1)
+R="\e[31m"
+G="\e[31m"
+N="\e[0m"
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 : $R failed $N "
+        exit 0
+    else
+        echo -e "$2 : $G sucees $N "
+    fi
+}
+if [ userid -ne 0]
+then
+    echo "please use root access"
+    exit 0
+else
+    echo " u are super user"
+fi
+
+for i in $@
+do 
+    echo " package to install : $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -e 0 ]
+    then
+        echo " already installed: $i"
+        exit 0
+    else    
+        echo "installing package : $i"
+        dnf install $i &>>$LOGFILE
+        validate $? "installing $i"
+    fi
+done
