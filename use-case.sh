264d8cc864867050fa4293685ca8fa5066b5d828
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-11-11 17:45:27 +0530

    use-case

--- use-case.sh
+++ use-case.sh
@@ -8,14 +8,14 @@ found_vowels=""
 
 for (( i=0; i<${#input_string}; i++ ));
 do
-    char=$input_string:$i:0
+    char="${input_string:i:1}"
     if [[ "$vowels" == *"$char"* ]]
     then
         found_vowels="$found_vowels$char "
     fi
 done
 
-if [ -n $found_vowels ]
+if [ -n "$found_vowels" ]
 then
     echo "vowels found in string : $found_vowels"
 else
