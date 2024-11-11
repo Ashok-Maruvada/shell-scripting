d27cfdff9f4d7b6fc2b3ba323794ef1121bf5e36
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-11-11 17:19:24 +0530

    use-case

---
+++ use-case.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+** Write a script to check vowels in a string **
+echo "enter any string: "
+read input_string
+
+vowels="aeiouAEIOU"
+found_vowels=""
+
+for (( i=0, i<len(input_string), i++ ));
+do
+    char=$input_string:$i:0
+    if [[ "$vowels" == *"$char"* ]]
+    then
+        found_vowels="$found_vowels$char "
+    fi
+done
+
+if [ -n $found_vowels ]
+then
+    echo "vowels found in string : $found_vowels"
+else
+    echo " no vowels found "
+fi
